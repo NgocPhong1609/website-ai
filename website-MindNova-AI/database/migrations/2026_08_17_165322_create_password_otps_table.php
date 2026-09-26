79d44b2d496d9924398e56bdca4222d979cdd3f6
Vũ Duy Hiếu <halilintarhieu@gmail.com>
2026-09-26 11:06:40 +0700

    chore: add nixpacks.toml to force migrate:fresh

--- website-MindNova-AI/database/migrations/2026_08_17_165322_create_password_otps_table.php
+++ website-MindNova-AI/database/migrations/2026_08_17_165322_create_password_otps_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        Schema::dropIfExists('password_otps');
         Schema::create('password_otps', function (Blueprint $table) {
             $table->id();
             $table->string('email')->index();
